36b2be67fd3921bfabc08ccfac36c85c5c51b07d
justin.fearn@snhu.edu <justin.fearn@snhu.edu>
2025-09-07 17:26:03 -0700

    Added playerChar BP for movement

---
+++ Source/Project_312/PlayerChar.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerChar.h"
+
+// Sets default values
+APlayerChar::APlayerChar()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Cam Setup
+	PlayerCamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("First Person Cam"));
+
+	//attach camera to mesh
+	PlayerCamComp->SetupAttachment(GetMesh(), "head");
+
+	//using controller rotation for character
+	PlayerCamComp->bUsePawnControlRotation = true;
+
+
+}
+
+// Called when the game starts or when spawned
+void APlayerChar::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerChar::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerChar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	//call base class to ensure parent setup is completed
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//bind axis and action mappings
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerChar::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerChar::MoveRight);
+	PlayerInputComponent->BindAxis("LookUp", this, &APlayerChar::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("Turn", this, &APlayerChar::AddControllerYawInput);
+	PlayerInputComponent->BindAction("JumpEvent", IE_Pressed, this, &APlayerChar::StartJump);
+	PlayerInputComponent->BindAction("JumpEvent", IE_Released, this, &APlayerChar::StopJump);
+
+
+}
+//handles forward.backward movement
+void APlayerChar::MoveForward(float axisValue)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, axisValue);
+}
+//handles right/left movement
+void APlayerChar::MoveRight(float axisValue)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Direction, axisValue);	
+}
+//called when jump button is pressed
+void APlayerChar::StartJump()
+{
+	bPressedJump = true;
+}
+
+void APlayerChar::StopJump()
+{
+	bPressedJump = false;
+}
+//Place holder for objects
+void APlayerChar::FindObject()
+{
+}
+
